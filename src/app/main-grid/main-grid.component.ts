d6a997d596db4a297fa5da9c716cc721b66aaaf4
Christian <chrisaug@rogers.com>
2018-09-18 16:25:18 -0400

    Fixed the image rotation issue buy creating an array of objects that contain the file name and the class name for file rotation

--- src/app/main-grid/main-grid.component.ts
+++ src/app/main-grid/main-grid.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 export interface Format {
   file: string;
-  rotate: string;
+  class: string;
 }
 
 @Component({
@@ -13,22 +13,22 @@ export interface Format {
 export class MainGridComponent implements OnInit {
 
   images: Format[] = [
-    {file: "IMG_5518.JPG", rotate: "0"},
-    {file: "IMG_5519.JPG", rotate: "0"},
-    {file: "IMG_5520.JPG", rotate: "-90"},
-    {file: "IMG_5530.JPG", rotate: "-90"},
-    {file: "IMG_5550.JPG", rotate: "0"},
-    {file: "IMG_5564.JPG", rotate: "-90"},
-    {file: "IMG_5599.JPG", rotate: "-90"},
-    {file: "IMG_5600.JPG", rotate: "-90"},
-    {file: "IMG_5638.JPG", rotate: "-90"},
-    {file: "IMG_5666.JPG", rotate: "-90"},
-    {file: "P1020259.JPG", rotate: "-90"},
-    {file: "P1020278.JPG", rotate: "0"},
-    {file: "P1020283.JPG", rotate: "0"},
-    {file: "P1030121.JPG", rotate: "-90"},
-    {file: "P1030152.JPG", rotate: "-90"},
-    {file: "P1030171.JPG", rotate: "-90"}
+    {file: "IMG_5518.JPG", class: "no-rotate-image"},
+    {file: "IMG_5519.JPG", class: "no-rotate-image"},
+    {file: "IMG_5520.JPG", class: "rotate-image"},
+    {file: "IMG_5530.JPG", class: "rotate-image"},
+    {file: "IMG_5550.JPG", class: "no-rotate-image"},
+    {file: "IMG_5564.JPG", class: "rotate-image"},
+    {file: "IMG_5599.JPG", class: "rotate-image"},
+    {file: "IMG_5600.JPG", class: "rotate-image"},
+    {file: "IMG_5638.JPG", class: "rotate-image"},
+    {file: "IMG_5666.JPG", class: "rotate-image"},
+    {file: "P1020259.JPG", class: "rotate-image"},
+    {file: "P1020278.JPG", class: "no-rotate-image"},
+    {file: "P1020283.JPG", class: "no-rotate-image"},
+    {file: "P1030121.JPG", class: "rotate-image"},
+    {file: "P1030152.JPG", class: "rotate-image"},
+    {file: "P1030171.JPG", class: "rotate-image"}
   ];
 
   //if rotate is "0" assign the class to noRotate and if ts "-90" the assign is rotate
